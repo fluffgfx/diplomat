89b7a1473da8c2f3435082c266aa9f06deedbb65
vulpine <hispanic@hush.ai>
2016-01-16 00:13:58 -0500

    Basic flag handling, working help command

--- diplomat.js
+++ diplomat.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict'
 /**
  * Created by Kyle Fahringer on 1/15/16.
  */
 
 class Diplomat {
-  constructor(args = []){
+  parse (args = []) {
     // first, we locate the command passed
 
     // remove node from the arguments, if the script was called using "node [script] [args]"
-    if (args[0] == 'node') {
+    if (args[0] === 'node') {
       args.shift()
     }
 
@@ -18,30 +18,107 @@ class Diplomat {
     // leaving just the arguments
     let command = args.shift()
 
-    if (typeof this[command] === 'function'){
-      // TODO: Parse the arguments further
-      this[command]()
+    if (typeof command === 'undefined') {
+      console.log('Please pass a command.')
+      return
+    }
+
+    if (typeof this[command] === 'function') {
+      let nextIsFlag = false
+      let nextFlag
+      let flags = {}
+      let opts = []
+      let command_flags = this[`${command}_flags`]()
+      args.forEach((o) => {
+        if (nextIsFlag) {
+          o = this.verify_type(o, command_flags[nextFlag]['type'])
+          if (o === 'error') {
+            console.log(`${o} is not a valid value for ${nextFlag}.`)
+            console.log(`Please refer to the help below for correct usage:`)
+            this.help()
+            return
+          }
+          flags[nextFlag] = o
+          nextIsFlag = false
+        } else if (o.includes('--')) {
+          o = o.replace('--', '')
+          if (typeof command_flags[o] !== 'undefined') {
+            nextIsFlag = true
+            nextFlag = o
+          } else {
+            console.log(`${o} is not a valid flag.`)
+            console.log(`Please refer to the help below for correct usage:`)
+            this.help()
+            return
+          }
+        } else {
+          opts.push(o)
+        }
+      })
+      if (nextIsFlag) {
+        console.log(`You didn't specify a value for ${nextFlag}.`)
+        console.log('Please refer to the help below for the correct usage:')
+        this.help()
+        return
+      }
+      this[command](opts, flags)
     } else {
-      console.log(`Sorry, but ${command} is not a command.`)
+      console.log(`${command} is not a command.`)
       console.log(`Please refer to the help below for correct usage:`)
       this.help()
+      return
     }
   }
 
-  help(args = {}){
-    let methods = Object.getOwnPropertyNames(this)
-    console.log(`Help for ${this.constructor.name}:`)
-    console.log(methods)
-    methods.forEach((m) => {
-      if (m === 'constructor' || (m.contains('help') && m != 'help')){
-        return
-      }
-      console.log(m)
-      console.log(`    ${m} - ${this[`${m}_help`]()['short']}`)
-    })
+  verify_type (o, type) {
+    switch (type) {
+      case 'boolean':
+        if (o === 'true') {
+          return true
+        } else if (o === 'false') {
+          return false
+        } else {
+          return 'error'
+        }
+        break
+      case 'string':
+        return o
+      case 'number':
+        if (isNaN(o)) {
+          return o
+        } else {
+          return 'error'
+        }
+        break
+      default:
+        return o
+    }
   }
 
-  help_help() {
+  help (args, flags) {
+    if (args) {
+      args.forEach((a) => {
+        console.log(`Help for ${this.constructor.name}.${a}:`)
+        console.log('---')
+        console.log(this[`${a}_help`]()['long'])
+        console.log('---')
+        console.log('Options:')
+        // TODO: Iterate over options
+      })
+    } else {
+      let methods = Object.getOwnPropertyNames(this.constructor.prototype)
+      console.log(`Help for ${this.constructor.name}:`)
+      methods.forEach((m) => {
+        if (m === 'constructor' || m === 'verify_type' || m.includes('help') || m.includes('flags')) {
+          return
+        }
+        console.log(`-    ${m}: ${this[`${m}_help`]()['short']}`)
+      })
+      console.log(`-    help: ${this['help_help']()['short']}`)
+    }
+  }
+
+  help_help () {
     return {
       short: 'Print this help screen.',
       long: 'This command helps you find the definitions for other commands.'
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 /**
  * Created by Kyle Fahringer on 1/15/16.
  */
@@ -6,16 +6,30 @@
 let diplomat = require('./diplomat.js')
 
 class Test extends diplomat {
-  test(){
-    console.log('Yay!')
+  test (args, flags) {
+    if (flags['sad']) {
+      console.log('Aww.')
+    } else {
+      console.log('Yay!')
+    }
   }
 
-  test_help(){
+  test_help () {
     return {
-      'short': 'Test',
-      'long': 'Hey its ok'
+      'short': 'This is a short bit of information.',
+      'long': 'This is a longer string. It will be displayed when you run help for a command.'
+    }
+  }
+
+  test_flags () {
+    return {
+      'sad': {
+        'type': 'boolean',
+        'default': 'false'
+      }
     }
   }
 }
 
-let test = new Test(process.argv)
+let test = new Test()
+test.parse(process.argv)
